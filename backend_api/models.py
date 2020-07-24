0b6d63b86fc4a21e8f0b9602b7f510cd59390468
mahanmmi <mahanmmi@gmail.com>
2020-07-24 23:48:46 +0430

    workshop prerequisites now can be optional

--- backend_api/models.py
+++ backend_api/models.py
@@ -94,7 +94,7 @@ class Workshop(models.Model):
     cost = models.PositiveIntegerField()
     desc = models.CharField(max_length=BIG_MAX_LENGTH)
     has_project = models.BooleanField(default=False, blank=False)
-    prerequisites = models.CharField(max_length=BIG_MAX_LENGTH, default='')
+    prerequisites = models.CharField(max_length=BIG_MAX_LENGTH, default='', blank=True)
     capacity = models.PositiveSmallIntegerField(default=50)
 
     NOT_ASSIGNED = 1
