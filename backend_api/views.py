228cc8a264fe67804329896355bdd64c88fe417f
mahanmmi <mahanmmi@gmail.com>
2020-07-30 11:30:32 +0430

    trying to fix a bug again

--- backend_api/views.py
+++ backend_api/views.py
@@ -154,7 +154,8 @@ class UserAPIView(APIView):
                     password='nothing'
                 )
             except IntegrityError:
-                user = models.User.objects.get(account__email=(serializer.validated_data.get('email').lower()))
+                a = str('an').lower()
+                user = models.User.objects.get(account__email=(str(serializer.validated_data.get('email')).lower()))
                 user_workshops = []
                 for workshop in user.registered_workshops.all():
                     user_workshops.append(workshop.id)
